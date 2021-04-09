1ba616ce1ff40ae592446bdfa1b42202b048f371
Axel <axel.rl.404@gmail.com>
2021-04-09 10:10:57 +0700

    update: updater also update current season anime

--- internal/tool/updater.go
+++ internal/tool/updater.go
@@ -2,8 +2,10 @@ package tool
 
 import (
 	"errors"
+	"strconv"
 	"time"
 
+	"github.com/rl404/go-malscraper/pkg/utils"
 	"github.com/rl404/mal-db/internal/constant"
 	"github.com/rl404/mal-db/internal/logger"
 	"github.com/rl404/mal-db/internal/model/raw"
@@ -36,9 +38,21 @@ func (m *Updater) Run() error {
 	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	currentMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
 
-	// Daily airing anime update.
+	season, seasonStart, seasonEnd := utils.GetCurrentSeason(), 0, 0
+	switch season {
+	case "winter":
+		seasonStart, seasonEnd = 1, 4
+	case "spring":
+		seasonStart, seasonEnd = 4, 7
+	case "summer":
+		seasonStart, seasonEnd = 7, 10
+	case "fall":
+		seasonStart, seasonEnd = 10, 13
+	}
+
+	// Daily airing or current season anime update.
 	var tmp raw.Anime
-	if !errors.Is(m.db.Select("id").Where("anime_status_id = ? and (updated_at < ? or updated_at is null)", 1, today).First(&tmp).Error, gorm.ErrRecordNotFound) {
+	if !errors.Is(m.db.Select("id").Where("(((premiered = '' and start_year = ? and start_month >= ? and start_month < ?) or (premiered != '' and split_part(premiered, ' ', 1) = ? and split_part(premiered, ' ', 2) = ?)) or anime_status_id = ?) and (updated_at < ? or updated_at is null)", now.Year(), seasonStart, seasonEnd, season, strconv.Itoa(now.Year()), 1, today).First(&tmp).Error, gorm.ErrRecordNotFound) {
 		return m.saver.Parse(constant.AnimeType, tmp.ID)
 	}
 
